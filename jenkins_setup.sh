da7435bf979b25539ec4ee1c415d08a0e18847f9
namtranvn <33787604+namtranvn@users.noreply.github.com>
2022-02-22 23:57:00 +0900

    Update jenkins_setup.sh

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
