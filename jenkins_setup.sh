73b988deeafd9c3bcc204a1194990537defee6bf
namtranvn <33787604+namtranvn@users.noreply.github.com>
2022-02-22 23:49:33 +0900

    Update jenkins_setup.sh

--- jenkins_setup.sh
+++ jenkins_setup.sh
@@ -1,1 +1,8 @@
-
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo amazon-linux-extras install java-openjdk11
+sudo yum install fontconfig java-11-openjdk
+sudo yum install jenkins
